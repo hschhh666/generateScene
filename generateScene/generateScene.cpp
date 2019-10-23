9cf0f25001d11f5334b499be344b3544767e8c46
hschhh666 <370228456@qq.com>
2019-10-23 22:42:53 +0800

    it can generate scene

---
+++ generateScene/generateScene.cpp
@@ -0,0 +1,143 @@
+﻿#include <iostream>
+#include "define.h"
+
+
+struct Position
+{
+	int x, y;
+};
+
+double sceneSize = 100;//meter
+double pixelSize = 0.2;//meter
+int imgSize = sceneSize / pixelSize;
+
+
+CvRect box;
+cv::Mat scene(imgSize, imgSize, CV_8UC3, cv::Scalar(255, 255, 255));
+cv::Mat tmp = scene.clone();
+
+bool buttonState = 0;
+
+int key = -1;
+
+std::vector<Position> targetPositions;
+
+
+void MouseCallback1(int event, int x, int y, int flag, void * param)
+{
+	int thinkness = -1;
+	scene = tmp.clone();
+
+	if (key == int('o')) 
+	{
+		switch (event)
+		{
+		case CV_EVENT_LBUTTONDOWN:
+			buttonState = 1;
+			box = cvRect(x, y, 0, 0);
+			std::cout << "CV_EVENT_LBUTTONDOWN\n";
+			break;
+		case CV_EVENT_MOUSEMOVE:
+			if (buttonState) {
+				CvRect tmpbox;
+				tmpbox.x = MIN(x, box.x);
+				tmpbox.y = MIN(y, box.y);
+				tmpbox.width = fabs(x - box.x);
+				tmpbox.height = fabs(y - box.y);
+				std::cout << "CV_EVENT_MOUSEMOVE\n";
+				cv::rectangle(scene, tmpbox, cv::Scalar(0, 0, 0), thinkness);
+			}
+			break;
+		case CV_EVENT_LBUTTONUP:
+			buttonState = 0;
+			CvRect tmpbox;
+			tmpbox.x = MIN(x, box.x);
+			tmpbox.y = MIN(y, box.y);
+			tmpbox.width = fabs(x - box.x);
+			tmpbox.height = fabs(y - box.y);
+			std::cout << "CV_EVENT_LBUTTONUP\n";
+			cv::rectangle(tmp, tmpbox, cv::Scalar(0, 0, 0), thinkness);
+			break;
+		}
+	}
+	else if (key == int('p') )
+	{
+		switch (event)
+		{
+		case CV_EVENT_LBUTTONDOWN:
+				Position p;
+				p.x = x;
+				p.y = y;
+				targetPositions.push_back(p);
+				std::cout << "CV_EVENT_LBUTTONDOWN\n";
+				//cv::circle(tmp, cv::Point(x, y), 4, cv::Scalar(0, 0, 255), -1);
+				break;
+		}
+	}
+}
+
+
+int main()
+{
+	std::string storagedPath = "D:/Research/IV2020MapPrediction/Code/MapPrediction/GenerateFakeMap/C++/EastGateScene";
+
+    std::cout << "Hello World!\n"; 
+	cv::namedWindow("Scene");
+	cv::setMouseCallback("Scene", MouseCallback1, 0);
+
+	while (1) {
+
+		for (int i = 0; i < targetPositions.size(); i++) {
+			cv::circle(scene, cv::Point(targetPositions[i].x, targetPositions[i].y), 4, cv::Scalar(0, 0, 255), -1);
+			cv::putText(scene, std::to_string(i), cv::Point(targetPositions[i].x + 5, targetPositions[i].y + 5), 1, 1, cv::Scalar(0, 0, 0));
+		}
+			
+		cv::putText(scene, "press 'o' to draw obstacle, 'p' to draw points, 'q' to quit.", cv::Point( 5, 15), 0, 0.5, cv::Scalar(0, 0, 255));
+		cv::imshow("Scene", scene);
+		int tmpkey = cv::waitKey(100);
+		key = (tmpkey != -1 ? tmpkey : key);
+
+		std::cout << key << std::endl;
+
+		if (key == int('q')) {
+
+			cv::FileStorage storage(storagedPath + ".yml", cv::FileStorage::WRITE);
+			if (!storage.isOpened()) {
+				std::cout << "Cannot write to " + storagedPath + ".yml \n Press any key to exit.\n";
+				getchar();
+				return(-1);
+			}
+
+			storage << "sceneSize" << sceneSize;
+			storage << "pixelSize" << pixelSize;
+
+			int size = targetPositions.size();
+			storage << "targetPositionsNum" << size;
+			for (int i = 0; i < targetPositions.size(); i++) {
+				storage << "targetPosition" + std::to_string(i) + "x" << targetPositions.at(i).x;
+				storage << "targetPosition" + std::to_string(i) + "y" << targetPositions.at(i).y;
+			}
+
+			storage << "structure" << tmp;
+			storage.release();
+
+			cv::destroyAllWindows();
+			
+			for (int i = 0; i < targetPositions.size(); i++)
+			{
+				cv::circle(tmp, cv::Point(targetPositions[i].x, targetPositions[i].y), 4, cv::Scalar(0, 0, 255), -1);
+				cv::putText(tmp, std::to_string(i), cv::Point(targetPositions[i].x + 5, targetPositions[i].y + 5), 1, 1, cv::Scalar(0, 0, 0));
+			}
+
+				
+			cv::imwrite(storagedPath + ".jpg", tmp);
+			std::cout << "Succeed write file to " + storagedPath + ".yml and jpg \nPress any key to exit.\n";
+			getchar();
+
+			break;
+		}
+
+	}
+
+}
+
---
+++ generateScene/opencvHeader.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+
+#if _DEBUG
+#pragma comment(lib, "D:/Softwares/Opencv340/build32/install/x86/vc15/lib/opencv_world340d.lib")
+
+
+#else
+#pragma comment(lib, "D:/Softwares/Opencv340/build32/install/x86/vc15/lib/opencv_world340.lib")
+#endif
